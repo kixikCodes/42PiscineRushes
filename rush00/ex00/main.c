c343d704d1ef8d4a6d0e2f2a85d352d59f4e1700
N03l-MG <noelmonzongarcia@gmail.com>
2025-08-04 20:22:53 +0200

    rush00 new bonus features (my unique approach)

--- rush00/ex00/main.c
+++ rush00/ex00/main.c
@@ -10,11 +10,50 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	rush(int x, int y);
+#include <unistd.h>
 
-int	main(void)
+#define X 5
+#define Y 5
+
+void	rush(int x, int y, void (*option)(int height, int width, int y, int x));
+void	put_pos_char00(int height, int width, int y, int x);
+void	put_pos_char01(int height, int width, int y, int x);
+void	put_pos_char02(int height, int width, int y, int x);
+void	put_pos_char03(int height, int width, int y, int x);
+void	put_pos_char04(int height, int width, int y, int x);
+
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	rush(5, 5);
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "Bonus: Please specifiy a printing option!\n", 42);
+		return (1);
+	}
+	if (!ft_strcmp(argv[1], "00"))
+		rush(X, Y, put_pos_char00);
+	else if (!ft_strcmp(argv[1], "01"))
+		rush(X, Y, put_pos_char01);
+	else if (!ft_strcmp(argv[1], "02"))
+		rush(X, Y, put_pos_char02);
+	else if (!ft_strcmp(argv[1], "03"))
+		rush(X, Y, put_pos_char03);
+	else if (!ft_strcmp(argv[1], "04"))
+		rush(X, Y, put_pos_char04);
+	else
+	{
+		write(1, "Error: Not a valid option.\n", 27);
+		return (1);
+	}
 	return (0);
 }
 
--- rush00/ex00/rush0X.c
+++ rush00/ex00/rush0X.c
@@ -31,77 +31,7 @@ bool	check_errors(int x, int y)
 	return (false);
 }
 
-// Option 00
-void	put_pos_char00(int height, int width, int y, int x)
-{
-	if ((width == 1 && height == 1) || (width == x && height == y)
-		|| (width == x && height == 1) || (width == 1 && height == y))
-		ft_putchar('o');
-	else if (height == 1 || height == y)
-		ft_putchar('-');
-	else if (width == 1 || width == x)
-		ft_putchar('|');
-	else
-		ft_putchar(' ');
-}
-
-// // Option 01
-// void	put_pos_char01(int height, int width, int y, int x)
-// {
-// 	if ((width == 1 && height == 1))
-// 		ft_putchar('/');
-// 	else if (!(y == 1 || x == 1) && (width == x && height == y))
-// 		ft_putchar('/');
-// 	else if ((width == x && height == 1) || (width == 1 && height == y))
-// 		ft_putchar('\\');
-// 	else if ((height == 1 || height == y) || (width == 1 || width == x))
-// 		ft_putchar('*');
-// 	else
-// 		ft_putchar(' ');
-// }
-
-// // Option 02
-// void	put_pos_char02(int height, int width, int y, int x)
-// {
-// 	if ((width == 1 && height == 1) || (width == x && height == 1))
-// 		ft_putchar('A');
-// 	else if ((width == 1 && height == y) || (width == x && height == y))
-// 		ft_putchar('C');
-// 	else if ((height == 1 || height == y) || (width == 1 || width == x))
-// 		ft_putchar('B');
-// 	else
-// 		ft_putchar(' ');
-// }
-
-// // Option 03
-// void	put_pos_char03(int height, int width, int y, int x)
-// {
-// 	if ((width == 1 && height == 1) || (width == 1 && height == y))
-// 		ft_putchar('A');
-// 	else if ((width == x && height == 1) || (width == x && height == y))
-// 		ft_putchar('C');
-// 	else if ((height == 1 || height == y) || (width == 1 || width == x))
-// 		ft_putchar('B');
-// 	else
-// 		ft_putchar(' ');
-// }
-
-// // Option 04
-// void	put_pos_char04(int height, int width, int y, int x)
-// {
-// 	if ((width == 1 && height == 1))
-// 		ft_putchar('A');
-// 	else if (!(y == 1 || x == 1) && (width == x && height == y))
-// 		ft_putchar('A');
-// 	else if ((width == x && height == 1) || (width == 1 && height == y))
-// 		ft_putchar('C');
-// 	else if ((height == 1 || height == y) || (width == 1 || width == x))
-// 		ft_putchar('B');
-// 	else
-// 		ft_putchar(' ');
-// }
-
-void	rush(int x, int y)
+void	rush(int x, int y, void (*option)(int height, int width, int y, int x))
 {
 	int	height;
 	int	width;
@@ -114,7 +44,7 @@ void	rush(int x, int y)
 	{
 		while (width <= x)
 		{
-			put_pos_char00(height, width, y, x);
+			option(height, width, y, x);
 			width++;
 		}
 		ft_putchar('\n');
